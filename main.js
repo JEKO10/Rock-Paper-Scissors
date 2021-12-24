74df86f9f1d13d0235c335e9d97431ddc97d8339
Aleksa Bubanja <aleksa3b@gmail.com>
2021-12-24 21:31:28 +0100

    rules section finished

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
+const info = document.querySelector(".info");
+const switchDiv = document.querySelector(".switch");
+const game = document.querySelector(".game");
 const rock = document.querySelector("#rock");
 const paper = document.querySelector("#paper");
 const sciss = document.querySelector("#sciss");
+const rules = document.querySelector(".rules");
+const rulesBtn = document.querySelector("#rulesBtn");
+const exitRules = document.querySelector("#exitRules");
 
-rock.addEventListener("click", () => {
-  console.log("asda");
+rulesBtn.addEventListener("click", () => {
+  rules.style.display = "block";
+  rulesBtn.style.display = "none";
+  info.style.display = "none";
+  switchDiv.style.display = "none";
+  game.style.display = "none";
+});
+
+exitRules.addEventListener("click", () => {
+  rules.style.display = "none";
+  info.style.display = "flex";
+  switchDiv.style.display = "flex";
+  game.style.display = "block";
+  rulesBtn.style.display = "block";
 });
