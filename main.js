1e5b6a3542f4d2b763c955f634a342a102648009
Aleksa Bubanja <aleksa3b@gmail.com>
2021-12-24 21:02:09 +0100

    rules button finished

---
+++ main.js
@@ -0,0 +1,7 @@
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const sciss = document.querySelector("#sciss");
+
+rock.addEventListener("click", () => {
+  console.log("asda");
+});
