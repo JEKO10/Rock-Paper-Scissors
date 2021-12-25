6ad51675228478258bc499b3a190076844691ad3
Aleksa Bubanja <aleksa3b@gmail.com>
2021-12-25 18:32:51 +0100

    user and computer setup

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const btns = document.querySelectorAll(".btn");
 const info = document.querySelector(".info");
 const switchDiv = document.querySelector(".switch");
 const game = document.querySelector(".game");
@@ -22,4 +23,22 @@ exitRules.addEventListener("click", () => {
   switchDiv.style.display = "flex";
   game.style.display = "block";
   rulesBtn.style.display = "block";
+});
+
+const symbol = [rock, paper, sciss];
+const random = Math.floor(Math.random() * 3);
+const computer = symbol[random];
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const user = e.target;
+    console.log(user);
+    if (user.id === "rock") {
+      console.log("rock");
+    } else if (user.id === "paper") {
+      console.log("paper");
+    } else {
+      console.log("sciss");
+    }
+  });
 });
